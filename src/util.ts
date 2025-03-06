c6002017162efab8f7c5494af18e2fbd57eaeb78
Tyler McCoy <tyler.mccoy@coalesce.io>
2025-03-06 13:43:03 -0700

    oops

--- src/util.ts
+++ src/util.ts
@@ -1,8 +1,7 @@
 import * as sh from "shelljs";
 
 export function executeCommand(isSilent: boolean, command: string) {
-  const result = sh.exec(command, { silent: isSilent, async: true });
-  return result.exitCode;
+  return sh.exec(command, { silent: isSilent }).code;
 }
 
 export function delay(ms: number) {
